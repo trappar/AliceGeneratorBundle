f0ff18498fb9d8ab65dbb51cb521a36d4a77668d
Jeff Way <jway@repx.me>
2016-06-08 18:23:16 -0700

    Fix method signature for test command

--- tests/SymfonyApp/TestBundle/Command/GenerateFixturesCommand.php
+++ tests/SymfonyApp/TestBundle/Command/GenerateFixturesCommand.php
@@ -3,6 +3,7 @@
 namespace Trappar\AliceGeneratorBundle\Tests\SymfonyApp\TestBundle\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Trappar\AliceGeneratorBundle\Command\AbstractFixtureGeneratorCommand;
 use Trappar\AliceGeneratorBundle\Tests\SymfonyApp\TestBundle\Entity\User;
 
@@ -19,7 +20,7 @@ class GenerateFixturesCommand extends AbstractFixtureGeneratorCommand
     /**
      * @inheritdoc
      */
-    public function getEntities(InputInterface $input)
+    public function getEntities(InputInterface $input, OutputInterface $output)
     {
         $user = new User();
         $user->setUsername('testUser');
